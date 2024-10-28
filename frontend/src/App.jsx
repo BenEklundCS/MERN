9e0e68035fbaeb1cd070b402d75c9d1e80944af3
Ben <eklundbenjaminj@gmail.com>
2024-10-28 16:13:36 -0700

    frontend changes in app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,18 +4,19 @@ import HomePage from "@/pages/HomePage";
 import CreatePage from "@/pages/CreatePage";
 import { Route, Routes } from 'react-router-dom';
 import Navbar from '@/components/Navbar';
+import { Box } from "@chakra-ui/react";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <Navbar>Navbar</Navbar>
+    <Box minH={"100vh"}>
+      <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
       </Routes>
-    </div>
+    </Box>
   )
 }
 
