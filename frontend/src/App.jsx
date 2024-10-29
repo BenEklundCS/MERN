6f3b5bcc63410c2f30ea00bddc08e42b3a0c763c
Ben <eklundbenjaminj@gmail.com>
2024-10-29 06:41:39 -0700

    CreatePage bugfix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
 			<Routes>
 				<Route path='/' element={<HomePage />} />
 				<Route path='/create' element={<CreatePage />} />
-        <Route path='/my_route' element={<MyPage />} />
+        		<Route path='/my_route' element={<MyPage />} />
 			</Routes>
 		</Box>
 	);
--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,4 +1,3 @@
-// src/pages/CreatePage.jsx
 import React from 'react';
 import { Container, VStack, Heading, useColorModeValue, Box, Input, Button } from '@chakra-ui/react';
 import { useProductStore } from '../../store/product';
@@ -10,24 +9,20 @@ const CreatePage = () => {
     image: "",
   });
 
-  const createProduct = useProductStore();
-  const handleAddProduct = async() => {
-    const {success, message} = createProduct(newProduct);
+  const { createProduct } = useProductStore(); // Destructure createProduct from the hook
+  const handleAddProduct = async () => {
+    const { success, message } = await createProduct(newProduct); // Await the async function
     console.log("success", success, message);
-  }
+  };
 
   return (
     <div>
       <Container maxW={"container.sm"}>
         <VStack spacing={8}>
-
-          <Heading as={"h1"} size="2x1" textAlign={"center"} mb={8}>
+          <Heading as={"h1"} size="2xl" textAlign={"center"} mb={8}>
             Create new product
           </Heading>
-
-          <Box w={"full"} bg={useColorModeValue("white", "gray.800")}
-            p = {6} rounded = {"lg"} shadow = {"md"}>
-
+          <Box w={"full"} bg={useColorModeValue("white", "gray.800")} p={6} rounded={"lg"} shadow={"md"}>
             <VStack spacing={4}>
               <Input
                 placeholder="Product name"
@@ -37,8 +32,8 @@ const CreatePage = () => {
               />
               <Input
                 placeholder="Product price"
-                name="name"
-                type='number'
+                name="price"
+                type="number"
                 value={newProduct.price}
                 onChange={(e) => setNewProduct({ ...newProduct, price: e.target.value })}
               />
@@ -48,13 +43,11 @@ const CreatePage = () => {
                 value={newProduct.image}
                 onChange={(e) => setNewProduct({ ...newProduct, image: e.target.value })}
               />
-              <Button colorScheme="blue" onClick={handleAddProduct} w='full'>
-                Add Product 
+              <Button colorScheme="blue" onClick={handleAddProduct} w="full">
+                Add Product
               </Button>
             </VStack>
-
           </Box>
-
         </VStack>
       </Container>
     </div>
