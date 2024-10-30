27f69229736f66113a09698ded90e31b1a5919e0
BenEklundCS <eklundbenjaminj@gmail.com>
2024-10-29 20:50:12 -0700

    project complete, basic CRUD demo with MERN

--- frontend/store/product.js
+++ frontend/store/product.js
@@ -46,5 +46,60 @@ export const useProductStore = create((set) => ({
             set((state) => ({ products: [...state.products, data.data] }));
             return { success: true, message: "Product created successfully" };
         }
-    }
+    },
+
+    fetchProducts: async () => {
+        // Send a GET request to the API to fetch all products
+        const res = await fetch("api/products");
+
+        // Parse the response data
+        const data = await res.json();
+
+        // If the request was successful, update the state with the products
+        if (res.ok) {
+            set({ products: data.data });
+        }
+    },
+
+    deleteProduct: async (pid) => {
+        const res = await fetch(`api/products/${pid}`, {
+            method: "DELETE"
+        });
+        const data = await res.json();
+        if (res.ok) {
+            // Update the state by removing the deleted product, this will trigger a re-render
+            set((state) => ({
+                products: state.products.filter((product) => product._id !== pid)
+            }));
+            return { success: true, message: data.message };
+        }
+    },
+
+    updateProduct: async (pid, updatedProduct) => {
+        const res = await fetch(`api/products/${pid}`, {  // Use backticks for interpolation
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(updatedProduct)
+        });
+    
+        const data = await res.json();
+    
+        if (res.ok) {
+            set((state) => ({
+                products: state.products.map((product) => {
+                    if (product._id === pid) {
+                        return { ...product, ...updatedProduct };
+                    }
+                    return product;
+                })
+            }));
+            return { success: true, message: data.message };
+        } else {
+            // Return an error message if the request was not successful
+            return { success: false, message: data.message || "Failed to update product" };
+        }
+    },
+    
 }));
